b3e2fe5c7bd90857ab651b5bc9603eaff2cbbd43
asamokhvalov <asamokhvalov@oneinc.com>
2023-10-17 09:26:15 -0600

    basic calculator - part-2

--- Problems/Stack/BasicCalculator.cs
+++ Problems/Stack/BasicCalculator.cs
@@ -13,7 +13,9 @@ public class BasicCalculator : ILeetCodeProblem
 
     public void Run()
     {
-        var s = "(-1+(-44+5+2)-3)+(6+8)"; // -1+-44+5 +2 -3+6+8
+        // 1 + 10
+        
+        var s = "10 + 20"; // -1+-44+5 +2 -3+6+8
         Console.WriteLine($"{s} = {Calculate(s)}");
     }
 
@@ -22,18 +24,42 @@ public class BasicCalculator : ILeetCodeProblem
     private  int Calculate(string s)
     {
         var calculationStack = new Stack<string>();
+        var str = s.Replace("(", string.Empty).Replace(")", string.Empty).Replace(" ", string.Empty);
+
+        for (var i = 0; i < str.Length; i++)
+        {
+            switch (s[i])
+            {
+                case '+':
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        var sign = 0;
+        
         foreach (var c in s.Where(x => char.IsDigit(x) || x == '+' || x == '-'))
         {
+            switch (c)
+            {
+                case '+':
+                    sign = 1;
+                    break;
+                default:
+                    sign = 0;
+                    break;
+            }
         }
 
-        return -1;
+        return Convert.ToInt32(calculationStack.Pop());
         
-        int Calculate(int rightValue, int leftValue, string operation)
+        int DoMath(string rightValue, string leftValue, string operation)
         {
             return operation switch
             {
-                "+" => rightValue + leftValue,
-                "-" => rightValue - leftValue,
+                "+" => Convert.ToInt32(rightValue) + Convert.ToInt32(leftValue),
+                "-" => Convert.ToInt32(rightValue) - Convert.ToInt32(leftValue),
                 _ => throw new NotSupportedException()
             };
         }
